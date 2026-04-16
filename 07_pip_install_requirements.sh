8991174eab19722e569dadf06cf89661cf34c7e4
y-ploni <7353755@gmail.com>
2026-04-16 15:58:15 +0300

    Locking the google-re2 directory

--- 07_pip_install_requirements.sh
+++ 07_pip_install_requirements.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 cd Sefaria-Project
-echo "google-re2==1.0" > constraints-ci.txt
+echo "google-re2==1.1.20240702" > constraints-ci.txt
 python -m pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt -c constraints-ci.txt
